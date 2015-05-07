3f84eb01720d3ce32b498211ff956a30a8c27779
mcaravind <blogsooner@gmail.com>
2015-05-06 22:15:37 -0700

    Path class

---
+++ ShortestPath/Path.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortestPath
+{
+    public class Path
+    {
+         
+    }
+
+    class Graph
+    {
+        public List<Vertex> Vertices;
+        public List<Edge> Edges;
+    }
+
+    class Vertex
+    {
+        public int Id;
+    }
+
+    class Edge
+    {
+        public Vertex v1;
+        public Vertex v2;
+        public int Weight;
+    }
+}
